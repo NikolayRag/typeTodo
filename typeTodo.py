f6305c43a31dfb44838c1fb04bece249dd7b25c0
NikolayRag <nikolayr@mail.ru>
2014-07-19 17:49:02 +0400

    cleanup: text and descriptions

--- typeTodo.py
+++ typeTodo.py
@@ -63,11 +63,8 @@ class TypetodoWwwCommand(sublime_plugin.TextCommand):
     def run(self, _edit):
         cDb= getDB(self.view)
         cCfg= cDb.cfgA
-        if cCfg['engine'] != 'http':
+        if cCfg['engine']!='http' or cCfg['addr']=='' or cCfg['base']=='':
             sublime.error_message('TypeTodo:\n\n\tProject is not configured for HTTP')
-            return
-        if cCfg['addr'] == '' or cCfg['base'] == '':
-            sublime.error_message('TypeTodo warning:\n\n\tCannot open project\'s HTTP repository.\n\tIt is not properly configured.')
             return
         webbrowser.open_new_tab('http://' +cCfg['addr'] +'/' +cCfg['base'] +'/' +cDb.projectName)
 
@@ -78,7 +75,7 @@ class TypetodoCfgOpenCommand(sublime_plugin.TextCommand):
         cDb= getDB(self.view)
         fn= os.path.join(cDb.projectRoot, cDb.projectName +'.do')
         if not os.path.isfile(fn):
-            sublime.message_dialog('TypeTodo:\n\tNo projects .do file found,\n\tplease restart Sublime')
+            sublime.message_dialog('TypeTodo:\n\tNo projects .do file,\n\tplease restart Sublime')
             return
         sublime.active_window().open_file(fn, sublime.TRANSIENT)
 
@@ -88,19 +85,19 @@ class TypetodoGlobalOpenCommand(sublime_plugin.TextCommand):
         cDb= getDB(False,'')
         fn= os.path.join(cDb.projectRoot, cDb.projectName +'.do')
         if not os.path.isfile(fn):
-            sublime.message_dialog('TypeTodo:\n\tCannot create global .do file,\n\tplease restart Sublime')
+            sublime.message_dialog('TypeTodo:\n\tNo global .do file,\n\tplease restart Sublime')
             return
         sublime.active_window().open_file(fn, sublime.TRANSIENT)
 
 class TypetodoGlobalResetCommand(sublime_plugin.TextCommand):
     def run(self, _edit):
         cDb= getDB(False,'')
-        if not sublime.ok_cancel_dialog('TypeTodo WARNING:\n\n\tGlobal .do file will be DELETED\n\tand created back with default settings\n\n\tIt may contain unsaved database\n\tconnection settings, such as login, pass\n\tor public repository name.\n\n\tProcceed anyway?'):
+        if not sublime.ok_cancel_dialog('TypeTodo WARNING:\n\n\tGlobal .do file will be DELETED\n\tand created back with default settings\n\n\tIt may contain unsaved database\n\tconnection settings, such as login, pass\n\tor public repository name.\n\n\tGlobal database content\n\twill be copied to new location.\n\n\tProcceed anyway?'):
             return
 
         cDb.flush(True)
         if not cDb.fetch() or not initGlobalDo(True):
-            sublime.message_dialog('TypeTodo:\n\tCannot reset global .do file,\n\tplease restart Sublime')
+            sublime.message_dialog('TypeTodo error:\n\tCannot reset global .do file,\n\tall remain intact.')
             return
 
         cDb.reset()
@@ -113,13 +110,12 @@ class TypetodoGlobalResetCommand(sublime_plugin.TextCommand):
 class TypetodoTransferCommand(sublime_plugin.TextCommand):
     def run(self, _edit):
         cDb= getDB(self.view)
-
-        if not sublime.ok_cancel_dialog('TypeTodo Transfer:\n\n\tCurrent project\'s TypeTodo settings\n\twill be reset from global config.\n\tAll content of project\'s database\n\twill be copied to new location.'):
+        if not sublime.ok_cancel_dialog('TypeTodo WARNING:\n\n\tCurrent project\'s TypeTodo settings\n\twill be reset using current global settings.\n\tProject\'s database content\n\twill be copied to new location.'):
             return
 
         cDb.flush(True)
         if not cDb.fetch():
-            sublime.error_message('TypeTodo Error:\n\n\tSafe fault, all remain intact.')
+            sublime.error_message('TypeTodo Error:\n\n\tCannot transfer,\n\tall remain intact.')
             return
 
 #todo 103 (command) +0: make fallback on transfer
@@ -128,7 +124,6 @@ class TypetodoTransferCommand(sublime_plugin.TextCommand):
             curTodo= cDb.todoA[iT].setSaved(False)
         cDb.dirty= True
         cDb.flush(True)
-
 
 
 class TypetodoEvent(sublime_plugin.EventListener):

0888a184b0c1fca73efe86d7aa09053c13cc50f9
NikolayRag <nikolayr@mail.ru>
2016-09-16 06:16:34 +0300

    fix: false commenting

--- c.py
+++ c.py
@@ -23,7 +23,7 @@ STATE_DEFAULT= ['', 'Pending']
 re_prefixes= ('<!--', '//', '#', '%', '\'', '!', ';', '--')
 RE_TODO_NEW= re.compile('(?P<prefix>.*?(?:' +'|'.join(re_prefixes) +'))todo(?P<trigger>:)?[ \t]*(?P<comment>.*)')
 RE_TODO_EXISTING= re.compile('(?P<prefix>.*?)(?:' +'|'.join(re_prefixes) +')\s*(?P<state>[\+\-\=\!]?)todo\s+(?P<id>\d+)(?:\s+\((?P<tags>.*)\))?(?:\s+(?P<priority>[\+\-]\d+))?\s*:(?P<postfix>[ \t]*(?P<comment>.*)[ \t]*)')
-#RE_TODO_INCONSISTENT= re.compile('(?P<prefix>.*?)(?:' +'|'.join(re_prefixes) +')\s*(?P<state>[\+\-\=\!]?)\s+(?P<id>\d+)(?:\s+\((?P<tags>.*)\))?(?:\s+(?P<priority>[\+\-]\d+))?\s*:(?P<postfix>[ \t]*(?P<comment>.*)[ \t]*)')
+RE_TODO_INCONSISTENT= re.compile('(?P<prefix>.*?)(?:' +'|'.join(re_prefixes) +')\s*(?P<state>[\+\-\=\!]?)    \s+(?P<id>\d+)(?:\s+\((?P<tags>.*)\))?(?:\s+(?P<priority>[\+\-]\d+))?\s*:(?P<postfix>[ \t]*(?P<comment>.*)[ \t]*)')
 RE_TODO_STORED= re.compile('^(?P<prefix>[\+\-\!\=])(?P<tags>.*) (?P<id>\d+): (?P<priority>[\+\-]\d+) (.+ \d\d/\d\d/\d\d \d\d:\d\d )?\"(?P<context>.*)\" (?P<editor>.+) (?P<editdate>\d\d/\d\d/\d\d) (?P<edittime>\d\d:\d\d)(?P<editsecs>:\d\d)?(?P<comment>)$')
 RE_TODO_STORED_COMMENT= re.compile('^\t?(?P<comment>.*)$')
 RE_TODO_FILE_MAXID= re.compile('^(?P<prefix>Reserved: )(?P<maxid>\d*)$')

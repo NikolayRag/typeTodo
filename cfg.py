89702127fa9ff6c05c7612da79a6c9eba0d6bb81
NikolayRag <nikolayr@mail.ru>
2017-03-20 06:49:36 +0300

    add check global settings at start

--- cfg.py
+++ cfg.py
@@ -69,6 +69,9 @@ class Config():
             self.projectFileName= os.path.join(self.projectRoot, self.projectName +'.do.cfg')
             self.projectLegacyFn= os.path.join(self.projectRoot, self.projectName +'.do')
 
+        #force check globals at start
+        self.initGlobalDo()
+
         self.update()
 
 
